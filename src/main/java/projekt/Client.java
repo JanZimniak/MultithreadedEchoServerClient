87dc5972d3b657512811e4aeb9287a07964627f6
Jan Zimniak <zimniak.jan@proton.me>
2026-04-17 22:11:44 +0200

    Added more strict close method for client

--- src/main/java/projekt/Client.java
+++ src/main/java/projekt/Client.java
@@ -1,12 +1,8 @@
 package projekt;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Scanner;
@@ -16,15 +12,17 @@ import java.util.concurrent.Executors;
 public class Client {
     private Socket socket;
     private ExecutorService executor;
-    private InetSocketAddress bindpoint;
     private InetSocketAddress endpoint;
+
+    private ObjectOutputStream write;
+    private ObjectInputStream read;
 
     private volatile boolean isRunning;
 
     public Client(String address, int port) throws IOException{
         this.socket = new Socket();
-        this.bindpoint = new InetSocketAddress(address, port);
-        this.socket.bind(this.bindpoint);
+        this.socket.setReuseAddress(true);
+        this.socket.bind(new InetSocketAddress(address, port));
         this.isRunning = false;
         this.executor = Executors.newCachedThreadPool(); 
     }
@@ -33,11 +31,14 @@ public class Client {
         this.endpoint = new InetSocketAddress(endAddress, endPort); 
         this.socket.connect(this.endpoint);
         this.isRunning = true;
+        this.write = new ObjectOutputStream(this.socket.getOutputStream());
+        this.read = new ObjectInputStream(this.socket.getInputStream());
         this.executor.submit(() -> handleSend());
         this.executor.submit(() -> handleReceive());
     }
 
-    public void close(){
+    public void closeClient(){
+        System.out.println("Closing client");
         this.isRunning = false;
         try{
             this.socket.close();
@@ -50,25 +51,29 @@ public class Client {
     private void handleSend(){
         try(
             Scanner scanner = new Scanner(System.in);
-            ObjectOutputStream write = new ObjectOutputStream(this.socket.getOutputStream());
            ){
 
             while(this.isRunning){
                 String input = scanner.nextLine();
+                if(input.equals("end")){
+                    closeClient();
+                    break;
+                }
                 DataObject sendData = DataObject.createMessage("client", input);
-                write.writeObject(sendData);
+                this.write.writeObject(sendData);
+                this.write.flush();
             } 
         }catch(IOException e){
-            System.out.println(e.getMessage());
+            if(this.isRunning){
+                System.out.println(e.getMessage());
+            }
         }
     }
 
     private void handleReceive(){
-        try(
-            ObjectInputStream read = new ObjectInputStream(this.socket.getInputStream());
-           ){
-            DataObject serverData;
-            while( (serverData = (DataObject)read.readObject()) != null ){
+        try{
+            while(this.isRunning){
+                DataObject serverData = (DataObject)this.read.readObject();
                 System.out.println(serverData.getMessage());
             }
         }catch(IOException e){
