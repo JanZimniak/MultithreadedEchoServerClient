d6566fa3c531b27a956a6ba598cd40b68522758f
Jan Zimniak <zimniak.jan@proton.me>
2026-04-17 20:52:04 +0200

    Added more strict close method for server

--- src/main/java/projekt/Server.java
+++ src/main/java/projekt/Server.java
@@ -1,15 +1,14 @@
 package projekt;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -18,47 +17,90 @@ public class Server {
     private ServerSocket socket;
     private volatile boolean isRunning;
     private ExecutorService executor;
-
     private InetSocketAddress bindpoint;
+
+    private Set<Socket> clientsSet;
 
     public Server(int port) throws IOException{
         this.socket = new ServerSocket();
         this.bindpoint = new InetSocketAddress(port);
         this.isRunning = false;
         this.executor = Executors.newCachedThreadPool(); 
+        this.clientsSet = ConcurrentHashMap.newKeySet();
     }
 
     public void start() throws IOException{
         this.socket.bind(this.bindpoint);
         this.isRunning = true;
+        this.executor.submit(()->{
+            getUserInput();
+        });
         acceptClients();
     }
 
-    private void acceptClients() throws IOException{
+    private void getUserInput(){
+        System.out.println("Press <ENTER> to stop server");
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+        sc.close();
+        closeServer();
+    }
+
+    private void closeServer(){
+        System.out.println("Closing server");
+        this.isRunning = false;
+
+        try{
+            this.socket.close();
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        for(Socket client : this.clientsSet){
+            try{
+                client.close();
+            }catch(IOException ignore){}
+        }
+        this.executor.shutdown();
+    }
+
+    private void acceptClients(){
         while(this.isRunning){
-            Socket clientSocket = this.socket.accept();
-            System.out.println("NEW CLIENT");
-            this.executor.submit(() -> handleClient(clientSocket)); 
+            try{
+                Socket clientSocket = this.socket.accept();
+                this.clientsSet.add(clientSocket);
+                System.out.println("NEW CLIENT " + clientSocket.getRemoteSocketAddress());
+                this.executor.submit(() -> handleClient(clientSocket)); 
+            }catch(IOException e){
+                if(this.isRunning){
+                    System.out.println(e.getMessage());
+                }
+                break;
+            }
         }
     }
 
     private void handleClient(Socket client){
         try(
-            ObjectInputStream fromClient = new ObjectInputStream(client.getInputStream());
             ObjectOutputStream writeToClient = new ObjectOutputStream(client.getOutputStream());
+            ObjectInputStream fromClient = new ObjectInputStream(client.getInputStream());
         ){
-            DataObject clientData;
-
-            while( (clientData = (DataObject)fromClient.readObject()) != null ){
+            while(this.isRunning){
+                DataObject clientData = (DataObject)fromClient.readObject(); 
                 String clientMessage = clientData.getMessage();
-                //System.out.println("FROM CLIENT: " + clientMessage);
                 DataObject echoData = DataObject.createMessage("server_echo", "ECHO "+clientMessage); 
                 writeToClient.writeObject(echoData);
+                writeToClient.flush();
             }
         }catch(IOException e){
             System.out.println("Client disconnected.");
         }catch(ClassNotFoundException e){
             System.out.println(e.getMessage());
+        }finally{
+            this.clientsSet.remove(client);
+            try{
+                client.close();
+            }catch(IOException ignore){}
         }
     }
     
