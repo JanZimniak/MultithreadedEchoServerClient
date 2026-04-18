f61c7e2cddfe06d4c297c9ceb3104249970fdcb8
Jan Zimniak <zimniak.jan@proton.me>
2026-04-18 20:43:12 +0200

    Removed ping, added reconnect when server is not found

--- src/main/java/projekt/Client.java
+++ src/main/java/projekt/Client.java
@@ -11,7 +11,12 @@ import java.util.concurrent.Executors;
 
 public class Client {
     private Socket socket;
+<<<<<<< HEAD
     private ExecutorService executor;
+=======
+    private ExecutorService executor = Executors.newCachedThreadPool();
+    private ScheduledExecutorService scheduled_executor = Executors.newSingleThreadScheduledExecutor();
+>>>>>>> 0ce6508 (Removed ping, added reconnect when server is not found)
     private InetSocketAddress endpoint;
 
     private ObjectOutputStream write;
@@ -19,9 +24,10 @@ public class Client {
 
     private volatile boolean isRunning;
 
-    public Client(String address, int port) throws IOException{
+    public Client() throws IOException{
         this.socket = new Socket();
         this.socket.setReuseAddress(true);
+<<<<<<< HEAD
         this.socket.bind(new InetSocketAddress(address, port));
         this.isRunning = false;
         this.executor = Executors.newCachedThreadPool(); 
@@ -35,6 +41,57 @@ public class Client {
         this.read = new ObjectInputStream(this.socket.getInputStream());
         this.executor.submit(() -> handleSend());
         this.executor.submit(() -> handleReceive());
+=======
+
+        this.isRunning = true;
+        this.isConnected = false;
+
+        this.executor.submit(this::handleSend);
+    }
+
+    public void connect(String endAddress, int endPort){
+        try{
+            this.endpoint = new InetSocketAddress(endAddress, endPort); 
+            startConnection();
+        }catch(IOException e){
+            reconnect();
+        }
+    }
+
+    private void reconnect(){
+        connectionCleanup();
+        this.scheduled_executor.scheduleAtFixedRate(()->{
+            if(!this.isConnected && this.isRunning){
+                try{
+                    System.out.println("RECONNECTING");
+                    startConnection();
+                }catch(IOException e){
+                    System.out.println("FAILED TO RECONNECT");
+                }
+            }
+        }, 2, 2, TimeUnit.SECONDS);
+    }
+    
+    public void startConnection() throws IOException{
+        connectionCleanup();
+        this.socket = new Socket();
+        this.socket.setReuseAddress(true);  
+        this.socket.connect(this.endpoint);
+        this.write = new ObjectOutputStream(this.socket.getOutputStream());
+        this.read = new ObjectInputStream(this.socket.getInputStream());
+        this.isConnected = true;
+        this.executor.submit(this::handleReceive);
+        System.out.println("CONNECTION SUCCESSFUL");
+    }
+
+    private void connectionCleanup(){
+        try{
+            if(this.socket != null){
+                this.socket.close();
+            }
+        }
+        catch(IOException ignore){}
+>>>>>>> 0ce6508 (Removed ping, added reconnect when server is not found)
     }
 
     public void closeClient(){
@@ -49,6 +106,7 @@ public class Client {
     }
 
     private void handleSend(){
+<<<<<<< HEAD
         try(
             Scanner scanner = new Scanner(System.in);
            ){
@@ -59,6 +117,20 @@ public class Client {
                     closeClient();
                     break;
                 }
+=======
+        Scanner scanner = new Scanner(System.in);
+        while(this.isRunning){
+            String input = scanner.nextLine();
+            if(input.equals("end")){
+                closeClient();
+                break;
+            }
+            if(!this.isConnected || this.write == null){
+                System.out.println("MESSAGE NOT SENT, NOT CONNECTED");
+                continue;
+            }
+            try{
+>>>>>>> 0ce6508 (Removed ping, added reconnect when server is not found)
                 DataObject sendData = DataObject.createMessage("client", input);
                 this.write.writeObject(sendData);
                 this.write.flush();
@@ -72,19 +144,29 @@ public class Client {
 
     private void handleReceive(){
         try{
-            while(this.isRunning){
+            while(this.isRunning && this.isConnected){
                 DataObject serverData = (DataObject)this.read.readObject();
                 System.out.println(serverData.getMessage());
             }
+<<<<<<< HEAD
         }catch(IOException e){
             System.out.println(e.getMessage());
         }catch(ClassNotFoundException e){
             System.out.println(e.getMessage());
+=======
+        }catch(IOException | ClassNotFoundException e){
+            if(!this.isRunning){
+                System.out.println("HANDLE_RECEIVE: " + e.getMessage());
+            }else{
+                this.isConnected = false;
+                reconnect();
+            }
+>>>>>>> 0ce6508 (Removed ping, added reconnect when server is not found)
         }
     }
 
     public static void main(String[] args) throws IOException {
-        Client client = new Client("localhost", 5555);
+        Client client = new Client();
         client.connect("127.0.0.1", 6666);
     }
 }
